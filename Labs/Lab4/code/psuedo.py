c9041574e1cc935b77ecd16e8772ad4356f58ba3
Samuel Guard <sg2268@bath.ac.uk>
2020-11-24 17:52:12 +0000

    Pretty much finished

---
+++ Labs/Lab4/code/psuedo.py
@@ -0,0 +1,24 @@
+infinity = 100000000000000000000000 #Representing infinity
+
+def minimax(node, alpha, beta, depth, player):
+    if(depth == MAX_DEPTH):
+        return estimateE(node.gameState) #Calculates E(x) for the game state represented by the current node
+
+    if(player == 1):
+        best = -infinity 
+        for n in nodes.children:
+            evalFuture = minimax(node, alpha, beta, depth - 1, 2)
+            alpha = max(evalFuture, alpha)
+            best = max(evalFuture, best)
+            if(alpha >= beta):
+                break
+        return best
+    else:
+        best = -infinity 
+        for n in nodes.children:
+            evalFuture = minimax(node, alpha, beta, depth - 1, 2)
+            beta = min(evalFuture, beta)
+            worst = max(evalFuture, worst)
+            if(alpha >= beta):
+                break
+        return worst
